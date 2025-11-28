4372e71aa14fafe0af7f7c39f35f79259e7837dc
catamr <223043964+Catamr@users.noreply.github.com>
2025-11-28 14:25:30 +1100

    Add ui functionality

--- script.js
+++ script.js
@@ -1,59 +1,68 @@
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
 
-function getComputerChoice(computerChoice) {
+const buttons = document.querySelectorAll("button");
+const pScoreDisplay = document.getElementById('playerScore');
+const cScoreDisplay = document.getElementById('computerScore');
+const imgDisplay = document.getElementById('gameImage');
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        if (button.id == "rock"){
+            playRound(1);
+        } else if (button.id == "paper"){
+            playRound(2);
+        } else if (button.id == "scissors"){
+            playRound(3);
+        };
+    });
+});
+
+function getComputerChoice() {
    c = Math.floor(Math.random() * 3) + 1;  
    return c 
 }
 
-function getHumanChoice(humanChoice){
-    i = prompt("Rock Paper Scissors!", "enter input");  
-  
-    if (i == "rock"){
-        r = 1
-    } else if (i == "paper"){
-        r = 2
-    } else {
-        r = 3
+function playRound(p){
+
+    c = getComputerChoice()
+    
+    if (c == p) {
+        imgDisplay.src = 'https://placehold.co/500x500/fde484/FFF/?text=Its+a+Draw'
+    } else if (c == 1 && p == 2) {
+        humanScore += 1
+        imgDisplay.src = 'https://placehold.co/500x500/b7f6cd/FFF/?text=Player+Scored'
+    } else if (c == 2 && p == 3) {
+        humanScore += 1 
+        imgDisplay.src = 'https://placehold.co/500x500/b7f6cd/FFF/?text=Player+Scored'
+    } else if (c == 3 && p ==1) {
+        humanScore += 1 
+        imgDisplay.src = 'https://placehold.co/500x500/b7f6cd/FFF/?text=Player+Scored'
+    } else{
+        computerScore += 1 
+        imgDisplay.src = 'https://placehold.co/500x500/b7f6cd/FFF/?text=Computer+Scored'
     }
-    return r
-}
 
-function playRound(){
-    c = getComputerChoice();
-    p = getHumanChoice(); 
-
-   if (c == p) {
-    return "draw" 
-   } else if (c == 1 && p == 2) {
-    humanScore += 1 
-    return "player wins"
-   } else if (c == 2 && p == 3) {
-    humanScore += 1 
-    return "player wins"
-   } else if (c == 3 && p ==1) {
-    humanScore += 1 
-    return "Player wins"
-   } else {
-    computerScore += 1 
-    return "computer wins"
-   }
-}
-
-function playGame(matchResult){ 
-    for (let i = 0; i < 5; i++){
-        playRound(); 
-    } 
-    console.log("player scored: " + humanScore)
-    console.log("computer scored: " + computerScore)
-    if (humanScore > computerScore){
-        return "Player Wins!!!!"
-    } else if (humanScore == computerScore) {
-        return "Draw"
+    if (computerScore >= 5){
+        gameOver("Computer Wins");
+    } else if (humanScore >= 5){
+        gameOver("Player wins!!!");
     } else {
-        return "Computer Wins!!!!"
+        pScoreDisplay.innerText = humanScore;
+        cScoreDisplay.innerText = computerScore;
+        return
     }
+
 }
 
-let play = playGame();
-console.log (play);
+function gameOver(winner){
+    humanScore = 0
+    computerScore = 0
+    pScoreDisplay.innerText = humanScore;
+    cScoreDisplay.innerText = computerScore;
+    alert(winner);
+
+}
+
+
